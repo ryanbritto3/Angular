7b141186db0f419adb6f74d939bd6bf732949803
Ghanshyam Gupta <gghanshyam01@gmail.com>
2018-08-21 03:32:16 +0530

    Recipes[] inside Service

--- src/app/recipe/handle-data.service.ts
+++ src/app/recipe/handle-data.service.ts
@@ -8,11 +8,33 @@ import { Recipe } from './models/recipe.model';
 })
 export class HandleDataService {
 
+  recipes: Recipe[] = [
+    new Recipe(
+      'Burger',
+      './assets/Burger.png',
+      'This is a western version of Indian Vada-Pav'
+    ),
+    new Recipe(
+      'Pizza',
+      './assets/Pizza.png',
+      // tslint:disable-next-line:max-line-length
+      'Pizza is a savoury dish of Italian origin, consisting of a round base dough topped with tomatoes, cheese, and meat'
+    ),
+    new Recipe(
+      'Cake',
+      './assets/Cake.png',
+      'Cake is a form of sweet dessert that is typically baked. In its oldest forms, cakes were modifications of breads'
+    )
+  ];
   recipeSource = new Subject<Recipe>();
   recipeObs$ = this.recipeSource.asObservable();
   constructor() { }
 
   updateRecipe(recipe: Recipe) {
     this.recipeSource.next(recipe);
+  }
+
+  getRecipes(): Recipe[] {
+    return [...this.recipes];
   }
 }
